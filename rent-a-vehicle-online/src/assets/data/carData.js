5ef36addc6daec5137ff6ba675c7e725469c095f
Harshit Khare <harshitkhare44@gmail.com>
2023-04-07 13:12:01 +0530

    added carData with images and all its details

--- rent-a-vehicle-online/src/assets/data/carData.js
+++ rent-a-vehicle-online/src/assets/data/carData.js
@@ -1,131 +1,132 @@
 // import all images from assets/images directory
-import img01 from "../all-images/cars-img/nissan-offer.png";
-import img02 from "../all-images/cars-img/offer-toyota.png";
-import img03 from "../all-images/cars-img/bmw-offer.png";
-import img04 from "../all-images/cars-img/nissan-offer.png";
-import img05 from "../all-images/cars-img/offer-toyota.png";
-import img06 from "../all-images/cars-img/mercedes-offer.png";
-import img07 from "../all-images/cars-img/toyota-offer-2.png";
-import img08 from "../all-images/cars-img/mercedes-offer.png";
+
+import img01 from "../all-images/cars-img/Royal Enfield Bullet.jpg";
+import img02 from "../all-images/cars-img/honda activa 5g.jpg";
+import img03 from "../all-images/cars-img/Bajaj Pulsar 220.jpg";
+import img04 from "../all-images/cars-img/Hero Glamour Bike.jpg";
+import img05 from "../all-images/cars-img/Ola Electric Scooter.jpg";
+import img06 from "../all-images/cars-img/Yamaha Fascino.webp";
+import img07 from "../all-images/cars-img/TVS Jupiter.png";
+import img08 from "../all-images/cars-img/TVS Raider 125.jpg";
 
 const carData = [
   {
     id: 1,
-    brand: "Tesla",
+    brand: "Royal Enfield",
     rating: 112,
-    carName: "Tesla Malibu",
+    carName: "Bullet",
     imgUrl: img01,
     model: "Model 3",
     price: 50,
     speed: "20kmpl",
     gps: "GPS Navigation",
     seatType: "Heated seats",
-    automatic: "Automatic",
+    automatic: "",
     description:
       " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
   },
 
   {
     id: 2,
-    brand: "Toyota",
+    brand: "Honda",
     rating: 102,
-    carName: "Toyota Aventador",
+    carName: "Honda Activa 5g",
     imgUrl: img02,
     model: "Model-2022",
     price: 50,
     speed: "20kmpl",
     gps: "GPS Navigation",
     seatType: "Heated seats",
-    automatic: "Automatic",
+    automatic: "",
     description:
       " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
   },
 
   {
     id: 3,
-    brand: "BMW",
+    brand: "Bajaj",
     rating: 132,
-    carName: "BMW X3",
+    carName: "Bajaj Pulsar 220",
     imgUrl: img03,
     model: "Model-2022",
     price: 65,
     speed: "20kmpl",
     gps: "GPS Navigation",
-    seatType: "Heated seats",
-    automatic: "Automatic",
+    seatType: "",
+    automatic: "",
     description:
       " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
   },
 
   {
     id: 4,
-    brand: "Nissan",
+    brand: "Hero",
     rating: 102,
-    carName: "Nissan Mercielago",
+    carName: "Hero Glamour",
     imgUrl: img04,
     model: "Model-2022",
     price: 70,
     speed: "20kmpl",
     gps: "GPS Navigation",
-    seatType: "Heated seats",
-    automatic: "Automatic",
+    seatType: " ",
+    automatic: "",
     description:
       " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
   },
 
   {
     id: 5,
-    brand: "Ferrari",
+    brand: "Ola",
     rating: 94,
-    carName: "Ferrari Camry",
+    carName: "Ola Elctric Scooter",
     imgUrl: img05,
     model: "Model-2022",
     price: 45,
     speed: "20kmpl",
     gps: "GPS Navigation",
-    seatType: "Heated seats",
-    automatic: "Automatic",
+    seatType: "",
+    automatic: "",
     description:
       " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
   },
 
   {
     id: 6,
-    brand: "Mercedes",
+    brand: "Yamaha",
     rating: 119,
-    carName: "Mercedes Benz XC90",
+    carName: "Yamhaha Fascino",
     imgUrl: img06,
     model: "Model-2022",
     price: 85,
     speed: "20kmpl",
     gps: "GPS Navigation",
-    seatType: "Heated seats",
-    automatic: "Automatic",
+    seatType: " ",
+    automatic: "",
     description:
       " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
   },
 
   {
     id: 7,
-    brand: "Audi",
+    brand: "TVS",
     rating: 82,
-    carName: "Audi Fiesta",
+    carName: "TVS Jupiter",
     imgUrl: img07,
     model: "Model 3",
     price: 50,
     speed: "20kmpl",
     gps: "GPS Navigation",
     seatType: "Heated seats",
-    automatic: "Automatic",
+    automatic: "",
     description:
       " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
   },
 
   {
     id: 8,
-    brand: "Colorado",
+    brand: "TVS",
     rating: 52,
-    carName: "Rolls Royce Colorado",
+    carName: "TVS Raider 125",
     imgUrl: img08,
     model: "Model 3",
     price: 50,
